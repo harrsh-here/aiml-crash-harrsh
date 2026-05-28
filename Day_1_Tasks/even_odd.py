0fc64355350e12058e198455662a95d341551f0a
Harsh Patidar <harshptidar1234@gmail.com>
2026-05-28 08:38:36 +0530

    feat(task-03): implement custom input parity mathematical filters

---
+++ Day_1_Tasks/even_odd.py
@@ -0,0 +1,13 @@
+﻿#Even Odd number program that handles invalid input.
+try:
+    num = int(input('Enter a Number : '))
+    if num==0:
+        ("Number is Zero!")
+    elif num%2==0:
+        print("EVEN")
+    else:
+        print("ODD")
+except Exception as e: 
+    print('Enter a valid number!!')
+    
+    
