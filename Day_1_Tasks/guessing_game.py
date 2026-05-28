287ccfb2557edbafc39fe74e74d0bc5be3f70445
Harsh Patidar <harshptidar1234@gmail.com>
2026-05-28 08:38:38 +0530

    feat(task-08): enforce execution constraints on pseudorandom simulators

---
+++ Day_1_Tasks/guessing_game.py
@@ -0,0 +1,21 @@
+﻿# Program to implement a simple random number guessing game.
+
+import random
+
+tgt, att, max_att = random.randint(1, 100), 0, 15
+
+while att < max_att:
+
+    try:
+        g = int(input(f"Guess ({att+1}/{max_att}): "))
+        att += 1
+        if g == tgt: 
+            print(f"Correct in {att} tries!"); 
+            break
+        print("too high" if g > tgt else "too low")
+
+    except ValueError: 
+        print("Enter numbers only.")
+
+else: 
+    print(f"Game over! Number was {tgt}.")
